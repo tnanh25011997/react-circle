03e0005489148c9a931eb5f71cbaaed034effc95
anhtn <tnanh25011997@gmail.com>
2021-11-28 18:19:08 +0700

    circle ci fix

--- src/App.js
+++ src/App.js
@@ -6,7 +6,18 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>alo</p>
+        <p>
+          Hello
+           
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
       </header>
     </div>
   );
