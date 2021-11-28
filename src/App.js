d55308dc38e19d94e6f337704eee57529834f061
anhtn <tnanh25011997@gmail.com>
2021-11-28 18:25:19 +0700

    circle ci fix

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Hello
-           
+           WTF
         </p>
         <a
           className="App-link"
